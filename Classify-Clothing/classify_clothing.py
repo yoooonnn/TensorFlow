dd5a4f5ebf331fa98238a0a2a7f53f9ba7889021
yoooonnn <yoooonnn@hanyang.ac.kr>
2024-11-19 09:05:44 +0900

    Preprocess #3

--- Classify-Clothing/classify_clothing.py
+++ Classify-Clothing/classify_clothing.py
@@ -12,3 +12,25 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 # 클래스 이름 (0~9)
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(train_images.shape)
+print(len(train_labels))
+
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
